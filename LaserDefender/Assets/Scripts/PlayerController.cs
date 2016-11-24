11b69e79c896fc8e33ed2bf519e6bc1c64b5c13d
Adan Gutierrez <hevyw8@gmail.com>
2016-11-23 22:03:31 -0600

    implemented player bounds

--- LaserDefender/Assets/Scripts/PlayerController.cs
+++ LaserDefender/Assets/Scripts/PlayerController.cs
@@ -3,21 +3,29 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
     public float speed = 15.0f;
+    public float padding = 1.0f;
 
+    private float xMin;
+    private float xMax;
 
-	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xMin = leftMost.x + padding;
+        xMax = rightMost.x - padding;
+    }	
+
 	void Update () {
 	    if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+            transform.position += Vector3.left * speed * Time.deltaTime;
         } else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            transform.position += Vector3.right * speed * Time.deltaTime;
         }
+        // restrict player to game space
+        float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
 	}
 }
