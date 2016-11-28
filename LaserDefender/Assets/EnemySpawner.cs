3ea30fc3e1f64387a159e82de3186070bc2231f9
Adan Gutierrez <hevyw8@gmail.com>
2016-11-28 14:01:53 -0600

    implemented enemyspawner

---
+++ LaserDefender/Assets/EnemySpawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+	
+
+	void Start () {
+        GameObject enemy = Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+        enemy.transform.parent = transform;
+    }
+	
+	
+	void Update () {
+	
+	}
+}
